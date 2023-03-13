d1d0a369824bf56bad09b18c318d3df8a7d3917a
Huiyu Yang <verthandi0105@gmail.com>
2023-03-13 19:02:08 +0800

    feat: 新增 account-login.ejs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import {urlencoded} from "express";
-
-const accountRoute = require('../src/presentation/routers/account-router')
-
+import {Client} from "pg";
 
 require('dotenv').config();
-console.log(process.env.PG_PASSWORD);
+
 const express = require('express');
 const app = express();
+const accountRoute = require('../src/presentation/routers/account-router')
+const PG_PORT: number = process.env.PG_PORT as unknown as number;
 
+
+const database = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    password: process.env.PG_PASSWORD,
+    port: PG_PORT,
+    })
+database.connect();
+
+app.set("view engine", "ejs");
 app.use(urlencoded({extended: true}));
 app.use(express.json());
-
 app.use('/account', accountRoute);
 app.listen(3000, () => {
     console.log("listening...")
--- src/presentation/routers/account-router.ts
+++ src/presentation/routers/account-router.ts
@@ -1,27 +1,24 @@
 import express, {Request, Response} from 'express';
-import {AccountService} from "../../account/account-service";
+// import {AccountService} from "../../account/account-service";
 
 const router = express.Router();
 
-export function AccountRouter(accountService: AccountService) {
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        return res.render("account-login");
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+})
 
-    router.get('/', async (req: Request, res: Response) => {
-        try {
-            const content = await accountService.findAll();
-            res.send(content);
-        } catch (e) {
-            res.status(500).send(e);
-        }
-    })
-    router.get('/:id', async (req: Request, res: Response) => {
-        try {
-            let {id} = req.params;
-            const content = await accountService.findOne(id);
-            res.send(content);
-        } catch (e) {
-            res.status(500).send(e);
-        }
-    })
-}
+// router.post('/',async (req: Request, res: Response) => {
+//     try {
+//         let {userID, userName, email, password, birthday} = req.body;
+//         let insertUser = await
+//     }catch (e){
+//
+//     }
+// })
+
 
 module.exports = router;
